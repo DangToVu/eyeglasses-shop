8436ca77774ca0fa6d76ad7241b5414eb695d62c
DangToVu <vuminhdang811@gmail.com>
2025-08-10 14:39:10 +0700

    add AllProducts btn to header and add confirm box for logout func

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
-// Header.jsx
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { BiCog } from "react-icons/bi";
+import ConfirmBox from "../components/ConfirmBox";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "../styles/components/Header.css";
 import logo from "../../public/logo.png";
 
 function Header() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
+  const [showLogoutConfirm, setShowLogoutConfirm] = useState(false);
   const [isVisible, setIsVisible] = useState(true);
   const navigate = useNavigate();
 
@@ -33,10 +36,20 @@ function Header() {
   }, []);
 
   const handleLogout = () => {
+    setShowLogoutConfirm(true);
+  };
+
+  const confirmLogout = () => {
     localStorage.removeItem("token");
     setIsLoggedIn(false);
     navigate("/");
     setShowDropdown(false);
+    setShowLogoutConfirm(false);
+    toast.success("Đăng xuất thành công"); // Thêm toast thông báo
+  };
+
+  const cancelLogout = () => {
+    setShowLogoutConfirm(false);
   };
 
   const handleManage = () => {
@@ -49,47 +62,68 @@ function Header() {
     setShowDropdown(false);
   };
 
+  const handleAllProducts = () => {
+    navigate("/products/all");
+    setShowDropdown(false);
+  };
+
   return (
-    <header className={`header ${isVisible ? "visible" : "hidden"}`}>
-      <div className="header-container">
-        <Link to="/" className="header-brand">
-          <img src={logo} alt="Kính mắt Tuân Hồng" className="header-logo" />
-        </Link>
-        <div className="header-nav">
-          <div
-            className="header-icon-wrapper"
-            onClick={() => setShowDropdown(!showDropdown)}
-          >
-            <BiCog className="header-icon" />
-            {showDropdown && (
-              <div className="dropdown-menu">
-                {isLoggedIn ? (
-                  <>
-                    <div className="dropdown-item" onClick={handleManage}>
-                      Quản lý
-                    </div>
-                    <div
-                      className="dropdown-item-logout"
-                      onClick={handleLogout}
-                    >
-                      Đăng xuất
-                    </div>
-                  </>
-                ) : (
+    <div>
+      {showLogoutConfirm && (
+        <ConfirmBox
+          message="Bạn có chắc chắn muốn đăng xuất không?"
+          onConfirm={confirmLogout}
+          onCancel={cancelLogout}
+        />
+      )}
+      <header className={`header ${isVisible ? "visible" : "hidden"}`}>
+        <div className="header-container">
+          <Link to="/" className="header-brand">
+            <img src={logo} alt="Kính mắt Tuân Hồng" className="header-logo" />
+          </Link>
+          <div className="header-nav">
+            <div
+              className="header-icon-wrapper"
+              onClick={() => setShowDropdown(!showDropdown)}
+            >
+              <BiCog className="header-icon" />
+              {showDropdown && (
+                <div className="dropdown-menu">
                   <Link
-                    to="/login"
+                    to="/products/all"
                     className="dropdown-item"
-                    onClick={handleLogin} // Sử dụng hàm riêng để điều khiển
+                    onClick={handleAllProducts}
                   >
-                    Đăng nhập
+                    Tất cả sản phẩm
                   </Link>
-                )}
-              </div>
-            )}
+                  {isLoggedIn ? (
+                    <>
+                      <div className="dropdown-item" onClick={handleManage}>
+                        Quản lý
+                      </div>
+                      <div
+                        className="dropdown-item-logout"
+                        onClick={handleLogout}
+                      >
+                        Đăng xuất
+                      </div>
+                    </>
+                  ) : (
+                    <Link
+                      to="/login"
+                      className="dropdown-item"
+                      onClick={handleLogin}
+                    >
+                      Đăng nhập
+                    </Link>
+                  )}
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 }
 
